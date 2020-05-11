7ff6dd9a77e875125da2933d9bb7becc864d139b
Pawan Gaur <pawangaur001@gmail.com>
2020-05-11 08:56:22 +0530

    Commit code for validate JWT Auth token

--- PPMToolFullStack/src/main/java/com/pgaur/ppmtool/security/JwtTokenProvider.java
+++ PPMToolFullStack/src/main/java/com/pgaur/ppmtool/security/JwtTokenProvider.java
@@ -1,8 +1,7 @@
 package com.pgaur.ppmtool.security;
 
 import com.pgaur.ppmtool.domain.User;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -39,7 +38,29 @@ public class JwtTokenProvider {
     }
 
     //validate the token
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex){
+            System.out.println("Invalid JWT Signature");
+        }catch (MalformedJwtException ex){
+            System.out.println("Invalid JWT Token");
+        }catch (ExpiredJwtException ex){
+            System.out.println("Expired JWT token");
+        }catch (UnsupportedJwtException ex){
+            System.out.println("Unsupported JWT token");
+        }catch (IllegalArgumentException ex){
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
 
     //get user id from token
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        String id = (String) claims.get("id");
+        return Long.parseLong(id);
+    }
 
 }
--- PPMToolFullStack/src/main/java/com/pgaur/ppmtool/security/SecurityConfig.java
+++ PPMToolFullStack/src/main/java/com/pgaur/ppmtool/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static com.pgaur.ppmtool.security.SecurityConstants.H2_URL;
 import static com.pgaur.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
@@ -21,6 +22,11 @@ import static com.pgaur.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
 
     @Autowired
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
@@ -66,6 +72,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(SIGN_UP_URLS).permitAll()
                 .antMatchers(H2_URL).permitAll()
                 .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
 }
